f569216b962457d7d84be95bd2c888a1b01e7a62
martin <martin.hurvois@gmail.com>
2026-03-25 10:40:00 +0100

    feat: config api tmdb cle base url et constantes image

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  API_KEY: 'c5734c655bfc25938ade445158f78d7d',
+  BASE_URL: 'https://api.themoviedb.org/3',
+  IMAGE_BASE_URL: 'https://image.tmdb.org/t/p',
+  POSTER_SIZE: 'w342',
+  BACKDROP_SIZE: 'w1280',
+  PROFILE_SIZE: 'w185',
+  LANGUAGE: 'fr-FR',
+  REGION: 'FR',
+};
